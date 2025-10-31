c11a0d358e210781360ff27ac5965fe1d7132b8a
Munkh-Orgil <munhorgilmunhtsetseg333@gmail.com>
2025-10-31 13:03:45 +0800

    10.31 js

---
+++ 10.31.js
@@ -0,0 +1,109 @@
+// 1deh bodlogo
+for (let index = 1; index <= 10; index++) {
+  console.log(index ** 2);
+}
+// 2dah bodlogo
+
+// 3dah bodlogo
+console.log(1896 % 4 === 0 && " mun");
+console.log(1896 % 4 === 1 || " bish");
+
+// 4deh bodlogo
+console.log(1924 % 4 === 0 && "mun");
+console.log(1924 % 4 === 1 || "bish");
+
+console.log(1994 % 2 === 0 && "mun");
+console.log(1994 % 2 === 1 || "bish");
+// 5 dahi bodlogo
+let n = 1;
+n1 = 2;
+n2 = 12;
+console.log(n2, n, n1, "uvul");
+n3 = 3;
+n4 = 4;
+n5 = 5;
+console.log(n3, n4, n5, "Khavar");
+n6 = 6;
+n7 = 7;
+n8 = 8;
+console.log(n6, n7, n8, "Zun");
+n9 = 9;
+n10 = 10;
+n11 = 11;
+console.log(n9, n10, n11, "Namar");
+// 6dahi bodlogo
+sum92 = 2500;
+sum95 = 3000;
+D = 2200;
+if ((a = 13.21))
+  if ((v = 10))
+    if ((s = 4)) {
+    }
+console.log("92:", a * sum92, "tugrug/litr");
+console.log("95:", v * sum95, "tugrug/litr");
+console.log("D:", s * D, "tugrug/litr");
+// 7dah bodlogo
+let tursun = 2005;
+let odoo = 2025;
+let nas = odoo - tursun;
+if (nas > 0 && nas <= 1) {
+  console.log(nas + "Infanf");
+} else if (nas > 1 && nas <= 3) {
+  console.log(nas + "Toddler");
+} else if (nas > 3 && nas <= 5) {
+  console.log(nas + "Preschooler");
+} else if (nas > 5 && nas <= 12) {
+  console.log(nas + "Gradeschooler");
+} else if (nas > 12 && nas <= 18) {
+  console.log(nas + "Teen");
+} else if (nas > 18 && nas <= 21) {
+  console.log(nas + " Young Adult");
+} else if (nas > 21) {
+  console.log(nas + "Adult");
+}
+// 8dah bod
+add = 0;
+for (let a = 1; a <= 5; a++) {
+  console.log(a ** 3);
+  a2 = a ** 3;
+  add = add += a2;
+}
+console.log(add, "batatlagdlaa");
+
+// 8iin uur nemelt arga
+let num = 153;
+one = (num % 10) ** 3;
+two = Math.floor((num % 100) / 10) ** 3;
+three = Math.floor(num / 100) ** 3;
+total = three + two + one;
+if (num === total) {
+  console.log("unen");
+} else {
+  console.log("hudal");
+}
+// 9dah bod
+for (let a = 0; a <= 9; a += 1) {
+  console.log(a);
+}
+
+// 10dah bod
+sum3 = 0;
+for (let a = 1; a <= 9; a++) {
+  console.log(a);
+  sum3 += a;
+}
+console.log(sum3, " bugd");
+
+sum4 = 0;
+for (let b = 1; b <= 9; b += 2) {
+  console.log(b);
+  sum4 += b;
+}
+console.log(sum4, " sondgoi niilber");
+
+sum5 = 0;
+for (let c = 2; c <= 9; c += 2) {
+  console.log(c);
+  sum5 += c;
+}
+console.log(sum5, "tegsh niilber");
